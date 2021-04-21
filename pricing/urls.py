fc35360acf4ce53823fb0922182d04c0c223711c
GrayUnit <grayunit@outlook.fr>
2021-04-21 19:07:22 +0200

    (Fin part2) utilisation d'un namespace dans les noms de views

--- pricing/urls.py
+++ pricing/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = "pricing"
 urlpatterns = [
     path('', views.index, name="index"),
     path('calculator/<int:id_vehicule>', views.calculator, name="calculator"),
