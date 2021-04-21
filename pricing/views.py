bd6f98ea7a2830ea6fa927ee7417e0af23aaa231
GrayUnit <grayunit@outlook.fr>
2021-04-21 18:44:47 +0200

    (Fin part1) Affichage des données dans la view

--- pricing/views.py
+++ pricing/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+from .models import Vehicule
 
 # Create your views here.
 def index(request):
     return HttpResponse("Bienvenue sur la page index")
 
 def calculator(request, id_vehicule):
-    return HttpResponse(f"Bienvenue sur la page calculator {id_vehicule}")
+    try:
+        vehicule = Vehicule.objects.get(pk=id_vehicule)
+    except Vehicule.DoesNotExist:
+        raise Http404("Vehicule introuvable")
+    else:
+        return HttpResponse(f"{vehicule.__dict__}")
